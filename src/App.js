ba323b62477e595060c22f5c7b7995ec3ba78a91
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2022-04-03 13:00:29 +0700

    add search input

--- src/App.js
+++ src/App.js
@@ -1,53 +1,106 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
 
 function App() {
   const [data, setData] = useState(null);
   const [totalRows, setTotalRows] = useState(0);
-	const [perPage, setPerPage] = useState(10)
+  const [perPage, setPerPage] = useState(10);
+
+  const [filterText, setFilterText] = useState('');
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 
   const fetchData = async (page) => {
-    const result = await fetch(`http://localhost:8080/api/v1/settings?size=${perPage}&page=${page - 1}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
-      },
-    }).then((res) => res.json());
+    const result = await fetch(
+      `http://localhost:8080/api/v1/settings?size=${perPage}&page=${page - 1}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
+        },
+      }
+    ).then((res) => res.json());
     setData(result);
     setTotalRows(result.totalElements);
   };
-  
-  const handlePerRowsChange = async (newPerPage, page) => {
-    const result = await fetch(`http://localhost:8080/api/v1/settings?size=${newPerPage}&page=${page - 1}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
-      },
-    }).then((res) => res.json());
 
-		setData(result);
-		setPerPage(newPerPage);
-	};
-  
-  const handlePageChange = page => {
-		fetchData(page);
-	};
-  
-  const handleSort = async (column, direction) => {
-    const result = await fetch(`http://localhost:8080/api/v1/settings?sort=${column.sortField},${direction}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
-      },
-    }).then((res) => res.json());
+  const handlePerRowsChange = async (newPerPage, page) => {
+    const result = await fetch(
+      `http://localhost:8080/api/v1/settings?size=${newPerPage}&page=${
+        page - 1
+      }`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
+        },
+      }
+    ).then((res) => res.json());
+
     setData(result);
-  }
+    setPerPage(newPerPage);
+  };
+
+  const handlePageChange = (page) => {
+    fetchData(page);
+  };
+
+  const handleSort = async (column, direction) => {
+    const result = await fetch(
+      `http://localhost:8080/api/v1/settings?sort=${column.sortField},${direction}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
+        },
+      }
+    ).then((res) => res.json());
+    setData(result);
+  };
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText('');
+      }
+    };
+
+    return (
+      <FilterComponent
+        onFilter={(e) => {
+          setFilterText(e.target.value);
+          filterData(e.target.value);
+        }}
+        onClear={() => {
+          handleClear();
+          filterData('');
+        }}
+        filterText={filterText}
+      />
+    );
+  }, [filterText, resetPaginationToggle]);
+
+  const filterData = async (query) => {
+    const result = await fetch(
+      `http://localhost:8080/api/v1/settings?query=${query}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmYXJtZXIiLCJyb2xlIjoiZmFybWVyIiwiZXhwIjoxNjUwMTY1MTM5LCJpYXQiOjE2NDg5NTU1MzksInVzZXJuYW1lIjoiZmFybWVyIn0.4MoT7Oo3WZ78jub9FKf_4Y_Tr435NaYo3Np5p8eSTz8',
+        },
+      }
+    ).then((res) => res.json());
+    setData(result);
+    setTotalRows(result.totalElements);
+  };
 
   useEffect(() => {
     fetchData();
@@ -67,27 +120,57 @@ function App() {
         onChangePage={handlePageChange}
         sortServer
         onSort={handleSort}
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
+        selectableRows
+        persistTableHead
       />
     </div>
   );
 }
 
+const FilterComponent = ({ filterText, onFilter, onClear }) => {
+  return (
+    <>
+      <form>
+        <input
+          id='search'
+          placeholder='filter by any column'
+          value={filterText}
+          onChange={onFilter}></input>
+        <button type='button' onClick={onClear}>
+          X
+        </button>
+      </form>
+    </>
+  );
+};
+
 const columns = [
   {
     name: 'id',
     selector: (row) => row.id,
+    width: '100px',
   },
   {
     name: 'key',
     selector: (row) => row.key,
     sortable: true,
-    sortField: 'key'
+    sortField: 'key',
   },
   {
     name: 'value',
     selector: (row) => row.value,
     sortable: true,
-    sortField: 'value'
+    sortField: 'value',
+    width: '200px',
+  },
+  {
+    name: 'description',
+    selector: (row) => row.description,
+    sortable: true,
+    sortField: 'description',
+    wrap: true,
   },
 ];
 
